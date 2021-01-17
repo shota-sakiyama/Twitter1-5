6d66e6e050c16ef23d6f0d7adc84b2e7795d2322
shota <#shota.gad1@gmail.com>
2021-01-17 21:26:34 +0900

    healoerメソッド編集

--- app/helpers/twitters_helper.rb
+++ app/helpers/twitters_helper.rb
@@ -1,2 +1,9 @@
 module TwittersHelper
+    def choose_create_or_edit
+        if action_name == 'new' || action_name == 'create'
+            confirm_twitters_path
+        elsif action_name == 'edit'
+            twitter_path
+        end
+    end
 end
